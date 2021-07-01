146a725a0cccc9632946454f31b516408556f116
FernandoOLI <oliveira-fernando1@hotmail.com>
2021-07-01 19:31:33 -0300

    Initial commit - first functions

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "teste"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/ImageMatrix.scala
@@ -0,0 +1,56 @@
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+import scala.Array.ofDim
+
+object ImageMatrix extends App {
+  val matrix = photoTomatrix(ImageIO.read(new File("src/main/resources/image.png")))
+  val matrixGray = toGray(matrix)
+
+  private def photoTomatrix(img: BufferedImage):Array[Array[Array[Int]]] = {
+    val w = img.getWidth
+    val h =  img.getHeight
+
+    var matrixRGBA = ofDim[Int](h, w, 4)
+    readImage(0, 0, img)
+
+    def readImage(w1: Integer, h1: Integer, img: BufferedImage): Unit = {
+      val rgb = img.getRGB(w1, h1)
+      val a = rgb >> 32 & 0xff
+      val r = rgb >> 16 & 0xff
+      val g = rgb >> 8 & 0xff
+      val b = rgb & 0xff
+
+      matrixRGBA(h1)(w1)(0) = a;
+      matrixRGBA(h1)(w1)(1) = r;
+      matrixRGBA(h1)(w1)(2) = g;
+      matrixRGBA(h1)(w1)(3) = b;
+      if (h1 < h - 1) {
+        readImage(w1, h1 + 1, img)
+      } else if (h1 == h - 1 && w1 < w - 1)
+        readImage(w1 + 1, 0, img)
+    }
+    matrixRGBA
+  }
+
+  def reduceImageMatrix(matrixRGBA: Array[Array[Array[Int]]]): Array[Array[Array[Int]]] = {
+    println("teste")
+    matrixRGBA
+  }
+
+  def toGray(matrixRGBA: Array[Array[Array[Int]]]): Array[Array[Double]] = {
+    val w = matrixRGBA.size
+    val h = matrixRGBA.length
+    var matrixGray = ofDim[Double](w, h)
+    readImage(0, 0, matrixRGBA)
+
+    def readImage(w1: Integer, h1: Integer, img: Array[Array[Array[Int]]]): Unit = {
+      matrixGray(h1)(w1) = 0.2989 * img(h1)(w1)(1) + 0.5870 * img(h1)(w1)(2) + 0.1140 * img(h1)(w1)(3)
+      if (h1 < h - 1)
+        readImage(w1, h1 + 1, img)
+      else if (h1 == h - 1 && w1 < w - 1)
+        readImage(w1 + 1, 0, img)
+    }
+    matrixGray
+  }
+}
