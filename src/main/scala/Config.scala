59d585365369ffbb574593111204c8623f863320
Fernando Oliveira <oliveira-fernando1@hotmail.com>
2021-07-16 16:13:29 -0300

    Add files via upload

---
+++ src/main/scala/Config.scala
@@ -0,0 +1,8 @@
+package imageTransform
+
+object Config {
+  val basewidth:Integer = 200
+  val baseheight:Integer = 100
+  val min_height:Integer = 5
+  val min_width:Integer = 5
+}
---
+++ src/main/scala/main.scala
@@ -0,0 +1,16 @@
+package imageTransform
+
+
+import imageTransform.ImageMatrix._
+
+import java.io.File
+import javax.imageio.ImageIO
+
+object main extends App {
+
+  val matrix = photoTomatrix(ImageIO.read(new File("src/main/resources/images/wall.jpg")))
+  val matrixGray = toGray(matrix)
+  saveGrayImage(matrixGray, "grayImage")
+  val matrixReduce = reduceImageMatrix(matrix)
+  saveGrayImage(toGray(matrixReduce),"reduceImage")
+}
